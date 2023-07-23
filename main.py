ea97d51477d8d154aa69b1176a28ca019d24245b
Praveen Kumar <75360904+praveen-learner@users.noreply.github.com>
2023-07-23 20:20:41 +0530

    Update main.py
    remove key

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 import telebot
 
-API_TOKEN = "6424545727:AAF_SJ7wbPeN4PP93tt7XaqvKSLNTNKQGOA"
+API_TOKEN = "tele-token"
 
 def summarize_article(article_url, length=5):
     """Summarizes an article and returns the summary."""
     url = "https://article-extractor-and-summarizer.p.rapidapi.com/summarize"
     headers = {
-        "X-RapidAPI-Key": "e87f5df8ddmshd4fdb9907c93964p1462d5jsn145babb65148",
+        "X-RapidAPI-Key": "rapidapi-token",
         "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com"
     }
     params = {
